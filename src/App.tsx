1c37e2f53d9182adbe76798ed4e5a0538e441fc3
Marco Zimmermann <fuzzyslash@hotmail.com>
2024-03-17 11:34:19 -0600

    Functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './App.css';
 import AdviceGenerator from './components/AdviceGenerator';
 
+const queryClient = new QueryClient();
+
 function App() {
-  return <AdviceGenerator />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AdviceGenerator />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
